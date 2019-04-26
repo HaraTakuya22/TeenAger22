1cda7dd492e4a628049269b89e00f9c8424a7881
HaraTakuya <taku-uver.rc-youtu@ezweb.ne.jp>
2019-04-26 11:08:57 +0900

    Add

---
+++ MAP/map_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="map2" tilewidth="80" tileheight="80" tilecount="480" columns="30">
+ <image source="map2.png" width="2400" height="1280"/>
+</tileset>

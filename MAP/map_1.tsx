9798094964acb53dc0dd3911915a1c8377c0179c
RyuseiKotou <ui6050.0402@ezweb.ne.jp>
2019-04-26 11:53:10 +0900

    map.png

--- MAP/map_1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="map" tilewidth="80" tileheight="80" tilecount="480" columns="30">
- <image source="map.png" width="2400" height="1280"/>
-</tileset>

aaa72bea528487aac8755ed0d8cc6a755b9f637b
MasatoHayashi <ma-yuto@ezweb.ne.jp>
2019-05-13 15:18:21 +0900

    Add

--- Source/TitleScene.cpp
+++ Source/TitleScene.cpp
@@ -56,14 +56,14 @@ unique_Base TitleScene::Update(unique_Base own, const Controller & controll)
 	}
 	startButtonCnt++;
 	// startButtonの表示の拡大縮小
-	if ((startButtonCnt/5)%20 == 1)
+	if ((startButtonCnt/5)%20 == 2)
 	{
 		startPos1 = { buttonLeftX -= START_BUTTON_DON,buttonLeftY -= START_BUTTON_DON };
 		startPos2 = { buttonRightX += START_BUTTON_DON,buttonLeftY -= START_BUTTON_DON };
 		startPos3 = { buttonRightX += START_BUTTON_DON,buttonRightY += START_BUTTON_DON };
 		startPos4 = { buttonLeftX -= START_BUTTON_DON ,buttonRightY += START_BUTTON_DON };
 	}
-	else if ((startButtonCnt / 5) % 20 == 2)
+	else if ((startButtonCnt / 5) % 20 == 1)
 	{
 		startPos1 = { buttonLeftX += START_BUTTON_DON,buttonLeftY += START_BUTTON_DON };
 		startPos2 = { buttonRightX -= START_BUTTON_DON,buttonLeftY += START_BUTTON_DON };
@@ -108,13 +108,13 @@ void TitleScene::Draw(void)
 		titleMapImage,
 		false);
 
-	//auto scr = lpScene.GetScrSize();
+	auto scr = lpScene.GetScrSize();
 	//DrawFormatString(600, 0, 0xffff00, "title");
 
-	//// Titleﾛｺﾞの配置位置(仮)
-	//DrawBox((scr.x / 2) - 150, (scr.y / 2) - 200, (scr.x / 2) + 150, (scr.y / 2) - 150,  0x0000ff, false);
+	// Titleﾛｺﾞの配置位置(仮)
+	DrawBox((scr.x / 2) - 500, (scr.y / 2) - 280, (scr.x / 2) + 500, (scr.y / 2) + 50,  0x0000ff, false);
 
-	//// ｹﾞｰﾑ選択文字の配置位置(仮)
+	// ｹﾞｰﾑ選択文字の配置位置(仮)
 	//for (int i = 20; i <= 80; i *= 4)
 	//{
 	//	DrawBox((scr.x / 2) - 80, ((scr.y / 2) - 40) + i, (scr.x / 2) + 80, (scr.y / 2) + i, 0x00ff00, false);
